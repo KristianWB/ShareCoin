3d7a558027f736a0c990fda5a19d7f82059e567a
Burhan Öztürk <burh0125@edu.easj.dk>
2019-05-20 11:51:53 +0200

    added Medlem

---
+++ app/src/main/java/com/example/sharecoin/Medlem.java
@@ -0,0 +1,71 @@
+package com.example.sharecoin;
+
+public class Medlem implements MedlemImpl {
+
+    private String virksomhedsnavn;
+    private int cvr;
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public int getTelefonNummer() {
+        return telefonNummer;
+    }
+
+    public void setTelefonNummer(int telefonNummer) {
+        this.telefonNummer = telefonNummer;
+    }
+
+    private String adresse;
+    private int telefonNummer;
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    private int password;
+
+    public String getVirksomhedsnavn() {
+        return virksomhedsnavn;
+    }
+
+    public void setVirksomhedsnavn(String virksomhedsnavn) {
+        this.virksomhedsnavn = virksomhedsnavn;
+    }
+
+    public int getCvr() {
+        return cvr;
+    }
+
+    public void setCvr(int cvr) {
+        this.cvr = cvr;
+    }
+
+    0
+
+    @Override
+    public void opretMedlemmer(String virksomhedsnavn, int cvr)
+    {
+
+    }
+
+    @Override
+    public void opdatereMedlemmer(String virksomhedsnavn, int cvr)
+    {
+
+    }
+
+    @Override
+    public void sletMedlemmer(int cvr) {
+
+    }
+}
