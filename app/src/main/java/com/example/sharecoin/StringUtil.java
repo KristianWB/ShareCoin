5941e3140cec5629e349fcd81a7fa46b078701ce
Burhan Öztürk <burh0125@edu.easj.dk>
2019-05-20 16:56:10 +0200

    added several more classes

--- app/src/main/java/com/example/sharecoin/StringUtil.java
+++ app/src/main/java/com/example/sharecoin/StringUtil.java
@@ -1,7 +1,14 @@
 package com.example.sharecoin;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.security.Key;
 import java.security.MessageDigest;
+import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.Signature;
+import java.util.Base64;
 
 public class StringUtil
 {
@@ -36,6 +43,88 @@ public class StringUtil
             throw new RuntimeException(e);
 
         }
+
+    }
+
+    public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
+
+        Signature dsa;
+
+        byte[] output = new byte[0];
+
+        try {
+
+            dsa = Signature.getInstance("ECDSA", "BC");
+
+            dsa.initSign(privateKey);
+
+            byte[] strByte = input.getBytes();
+
+            dsa.update(strByte);
+
+            byte[] realSig = dsa.sign();
+
+            output = realSig;
+
+        } catch (Exception e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+        return output;
+
+    }
+
+
+
+    //Verifies a String signature
+
+    public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
+
+        try {
+
+            Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+
+            ecdsaVerify.initVerify(publicKey);
+
+            ecdsaVerify.update(data.getBytes());
+
+            return ecdsaVerify.verify(signature);
+
+        }catch(Exception e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String getStringFromKey(Key key) {
+
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+
+    }
+
+
+    public void generateSignature(PrivateKey privateKey) {
+
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+
+        signature = StringUtil.applyECDSASig(privateKey,data);
+
+    }
+
+//Verifies the data we signed hasnt been tampered with
+
+    public boolean verifiySignature() {
+
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+
+        return StringUtil.verifyECDSASig(sender, data, signature);
 
     }
 
--- app/src/main/java/com/example/sharecoin/Wallet.java
+++ app/src/main/java/com/example/sharecoin/Wallet.java
@@ -1,6 +1,8 @@
 package com.example.sharecoin;
 import java.security.*;
 import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class Wallet
 {
@@ -46,4 +48,99 @@ public class Wallet
 
     }
 
-}
+    public PrivateKey privateKey;
+
+    public PublicKey publicKey;
+
+
+
+    public HashMap<String,TransactionOutput> UTXOs = new HashMap<String,TransactionOutput>(); //only UTXOs owned by this wallet.
+
+
+
+    public Wallet() {...
+
+
+
+        public void generateKeyPair() {...
+
+
+
+            //returns balance and stores the UTXO's owned by this wallet in this.UTXOs
+
+            public float getBalance() {
+
+                float total = 0;
+
+                for (Map.Entry<String, TransactionOutput> item: NoobChain.UTXOs.entrySet()){
+
+                    TransactionOutput UTXO = item.getValue();
+
+                    if(UTXO.isMine(publicKey)) { //if output belongs to me ( if coins belong to me )
+
+                        UTXOs.put(UTXO.id,UTXO); //add it to our list of unspent transactions.
+
+                        total += UTXO.value ;
+
+                    }
+
+                }
+
+                return total;
+
+            }
+
+            //Generates and returns a new transaction from this wallet.
+
+            public Transaction sendFunds(PublicKey PublicKey _recipient;
+            _recipient,float value ) {
+
+                if(getBalance() < value) { //gather balance and check funds.
+
+                    System.out.println("#Not Enough funds to send transaction. Transaction Discarded.");
+
+                    return null;
+
+                }
+
+                //create array list of inputs
+
+                ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+
+
+
+                float total = 0;
+
+                for (Map.Entry<String, TransactionOutput> item: UTXOs.entrySet()){
+
+                    TransactionOutput UTXO = item.getValue();
+
+                    total += UTXO.value;
+
+                    inputs.add(new TransactionInput(UTXO.id));
+
+                    if(total > value) break;
+
+                }
+
+
+
+                Transaction newTransaction = new Transaction(publicKey, _recipient , value, inputs);
+
+                newTransaction.generateSignature(privateKey);
+
+
+
+                for(TransactionInput input: inputs){
+
+                    UTXOs.remove(input.transactionOutputId);
+
+                }
+
+                return newTransaction;
+
+            }
+
+
+
+        }
