6c858a98acdd93cb8e97b5d56116d0f8739b6a50
Burhan Öztürk <burh0125@edu.easj.dk>
2019-05-20 11:49:13 +0200

    added MedlemImpl and edited

--- app/src/main/java/com/example/sharecoin/MedlemImpl.java
+++ app/src/main/java/com/example/sharecoin/MedlemImpl.java
@@ -1,5 +1,8 @@
 package com.example.sharecoin;
 
 public interface MedlemImpl {
-    
+    void opretMedlemmer(String virksomhedsnavn,int cvr);
+    void opdatereMedlemmer(String virksomhedsnavn,int cvr);
+    void sletMedlemmer(int cvr);
+
 }
